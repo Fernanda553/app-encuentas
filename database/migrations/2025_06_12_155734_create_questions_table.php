ba3fbc4f30e63b6480e78624604e0c82c3b2f265
fernanda nuñez <fernanda.nunez55@gmail.com>
2025-06-12 12:10:13 -0400

    Add migration order to ensure correct creation of surveys, questions, and answers tables

---
+++ database/migrations/2025_06_12_155734_create_questions_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('survey_id')->constrained()->cascadeOnDelete();
+            $table->string('text');
+            $table->enum('type', ['single', 'multiple'])->default('single');
+            $table->integer('order_column')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('questions');
+    }
+};
