e4987c7471479609e5b14b9bf9eaba04300e9b28
fernanda nuñez <fernanda.nunez55@gmail.com>
2025-06-12 13:32:36 -0400

    Update VoteCasted event to use ShouldBroadcastNow and add broadcastWith method

--- app/Events/VoteCasted.php
+++ app/Events/VoteCasted.php
@@ -5,13 +5,11 @@ namespace App\Events;
 use App\Models\Survey;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PresenceChannel;
-use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class VoteCasted implements ShouldBroadcast
+class VoteCasted implements ShouldBroadcastNow
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
@@ -25,4 +23,9 @@ class VoteCasted implements ShouldBroadcast
             new Channel('survey.' . $this->survey->id),
         ];
     }
-} 
+
+    public function broadcastWith(): array
+    {
+        return ['survey' => $this->survey->toArray()];
+    }
+}
